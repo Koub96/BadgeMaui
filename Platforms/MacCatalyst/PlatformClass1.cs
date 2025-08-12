c13cdaf110f2aa8c0f88a059a8e9040cbc7ba12d
Konstantinos Koumpanakis <kko@entersoft.gr>
2025-08-12 20:47:30 +0300

    Initial commit - .NET MAUI project

---
+++ Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace BadgeMaui
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace BadgeMaui
+{
+    // All the code in this file is only included on Windows.
+    public class PlatformClass1
+    {
+    }
+}
